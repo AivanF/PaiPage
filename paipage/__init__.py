c7b2752dcaf2b127c2900ea9a367da19ccfe1c2e
Aivan <AivanF@users.noreply.github.com>
2020-06-06 23:21:36 +0300

    Config improvements

--- paipage/__init__.py
+++ paipage/__init__.py
@@ -4,77 +4,70 @@ __contact__ = 'projects@aivanf.com'
 
 import os, sys
 
-from .const import REQUIRED_STRINGS
-
-config = None
+from .const import REQUIRED_STRINGS, Language
 
 
-class Language:
-	def __init__(self, code, strings):
-		self.code = code
-		for key in REQUIRED_STRINGS:
-			assert key in strings, f'No string "{key}" for lang "{code}"'
-		self.strings = strings
+class Config:
+	def __init__(self):
+		pass
 
 
-class Configurate:
-	def __init__(self,
-			settings_name,
-			site_name,
-			template_page_default='pg-usual',
-			template_layout_default='lo-menu-top',
-			language_default=None,
-			language_available=None,
-			logger=None,
-			):
-		global config
-		if config is not None:
-			raise ValueError(f'PaiPage config collision')
-		config = self
-
-		assert isinstance(site_name, str) and len(site_name) > 0
-		self.site_name = site_name
-
-		self.template_page_default = template_page_default
-		self.template_layout_default = template_layout_default
-
-		for x in language_available:
-			assert isinstance(x, Language)
-		self.language_available = {x.code: x for x in language_available}
-		assert isinstance(language_default, str)
-		assert language_default in self.language_available
-		self.language_default = language_default
-
-		if logger is None:
-			import logging
-			logger = logging.getLogger('paipage')
-			logger.setLevel(logging.DEBUG)
-			console_handler = logging.StreamHandler(sys.stdout)
-			formatter = logging.Formatter('%(asctime)s %(levelname)s: %(message)s')
-			console_handler.setFormatter(formatter)
-			logger.addHandler(console_handler)
-		self.logger = logger
+config = Config()
 
 
-		# django.conf.settings isn't set yet
-		settings = sys.modules[settings_name]
+def configurate(
+		settings_name,
+		site_name,
+		template_page_default='pg-usual',
+		template_layout_default='lo-menu-top',
+		language_default=None,
+		language_available=None,
+		logger=None,
+		):
 
-		APP_DIR = os.path.dirname(os.path.abspath(__file__))
+	assert isinstance(site_name, str) and len(site_name) > 0
+	config.site_name = site_name
 
-		settings.INSTALLED_APPS.append('paipage')
+	config.template_page_default = template_page_default
+	config.template_layout_default = template_layout_default
 
-		settings.TEMPLATES.append({
-			'BACKEND': 'django.template.backends.jinja2.Jinja2',
-			'DIRS': [os.path.join(APP_DIR, 'jemplates')],
-			'APP_DIRS': True,
-			'OPTIONS': {
-				'environment': 'paipage.views.jinja2.make_env',
-				# 'autoescape': False,
-			}, 
-		})
+	for x in language_available:
+		assert isinstance(x, Language)
+	config.language_available = {x.code: x for x in language_available}
+	assert isinstance(language_default, str)
+	assert language_default in config.language_available
+	config.language_default = language_default
 
-		settings.SESSION_ENGINE = 'django.contrib.sessions.backends.signed_cookies'
+	if logger is None:
+		import logging
+		logger = logging.getLogger('paipage')
+		logger.setLevel(logging.DEBUG)
+		console_handler = logging.StreamHandler(sys.stdout)
+		formatter = logging.Formatter('%(asctime)s %(levelname)s: %(message)s')
+		console_handler.setFormatter(formatter)
+		logger.addHandler(console_handler)
+	config.logger = logger
 
-		# TODO: get installed/selected plugins info
-		# TODO: collect static/templates from plugins
-		# TODO: make available templates list
+
+	# django.conf.settings isn't set yet
+	settings = sys.modules[settings_name]
+
+	APP_DIR = os.path.dirname(os.path.abspath(__file__))
+
+	settings.INSTALLED_APPS.append('paipage')
+
+	settings.TEMPLATES.append({
+		'BACKEND': 'django.template.backends.jinja2.Jinja2',
+		'DIRS': [os.path.join(APP_DIR, 'jemplates')],
+		'APP_DIRS': True,
+		'OPTIONS': {
+			'environment': 'paipage.views.jinja2.make_env',
+			# 'autoescape': False,
+		}, 
+	})
+
+	settings.SESSION_ENGINE = 'django.contrib.sessions.backends.signed_cookies'
+
+	# TODO: get installed/selected plugins info
+	# TODO: collect static/templates from plugins
+	# TODO: make available templates list
--- paipage/const.py
+++ paipage/const.py
@@ -13,3 +13,33 @@ REQUIRED_STRINGS = (
 	'errorNoLang',  # for pages with no text in current lang
 	'back',  # "Back" link
 )
+
+
+class Language:
+	def __init__(self, code, strings):
+		self.code = code
+		for key in REQUIRED_STRINGS:
+			assert key in strings, f'No string "{key}" for lang "{code}"'
+		self.strings = strings
+
+
+lang_en = Language(
+	code='en',
+	strings=dict(
+		name='English',
+		languages='Other languages',
+		error404='Page not found',
+		errorNoLang='Not available in this language',
+		back='Back',
+	),
+)
+lang_ru = Language(
+	code='ru',
+	strings=dict(
+		name='Русский',
+		languages='Другие языки',
+		error404='Страница не найдена',
+		errorNoLang='Не доступно на этом языке',
+		back='Назад',
+	),
+)
