593cd62529f979f4a38e0bd2803cd4d3a6498c54
Aivan <AivanF@users.noreply.github.com>
2020-11-09 23:26:20 +0300

    Setup.py: with non-Python files

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as f:
 
 setuptools.setup(
     name='PaiPage',
-    version='1.0.0',
+    version='1.0.3',
     author='AivanF.',
     author_email='projects@aivanf.com',
     description='A small Python library to deal with big tables',
@@ -13,6 +13,7 @@ setuptools.setup(
     long_description_content_type='text/markdown',
     url='https://github.com/AivanF/PaiPage',
     packages=setuptools.find_packages(),
+    include_package_data=True,
     classifiers=[
         'Development Status :: 4 - Beta',
         'Programming Language :: Python :: 3',
